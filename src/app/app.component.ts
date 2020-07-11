0751c1daaf47fa2fb8e3160ed53c649292cc6976
DXF0530 <dmitry.fleish@sbdinc.com>
2020-07-11 20:58:43 +0300

    commit-110720202058

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, shareReplay, startWith, tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'summer-of-angular-one-part-two';
+  repos;
+
+  constructor(private http: HttpClient) {
+    const path = 'http://api.github.com/search/repositories?q=angular';
+    this.repos = http.get<any>(path)
+      .pipe(map(list => list.items),
+        shareAndCache('github-repolist'));
+  }
+}
+
+export function shareAndCache<T>(storageKey: string): (source: Observable<T>) => Observable<T> {
+  return (source: Observable<T>) => {
+    return source.pipe(
+      tap(value => localStorage[storageKey] = JSON.stringify(value)),
+      startWith(JSON.parse(localStorage[storageKey] || '[]')),
+      shareReplay(1)
+    );
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
